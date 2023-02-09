fa3b957dd888c6ac5ba6f5950039d5e0f7b6227b
Stat-sid <98740788+Stat-sid@users.noreply.github.com>
2023-02-09 09:55:50 +0100

    Database design changes

--- DIT621 V23 Databases/Assignment/Part 1/views.sql
+++ DIT621 V23 Databases/Assignment/Part 1/views.sql
@@ -29,37 +29,41 @@ FROM waitingList AS w;
 
 
 CREATE VIEW UnreadMandatory AS
-(SELECT s.idnr AS student, mp.course
-FROM MandatoryProgram AS mp, Students AS s WHERE s.program = mp.program 
+(SELECT basicInformation.idnr AS student, mp.course
+FROM MandatoryProgram AS mp
+natural JOIN basicInformation
 UNION
-SELECT s.idnr AS student, mb.course
-FROM MandatoryBranch AS mb, Students AS s)
+SELECT basicInformation.idnr AS student, mb.course 
+FROM MandatoryBranch AS mb
+natural join basicInformation)
 EXCEPT
-SELECT DISTINCT p.student, p.course
+SELECT p.student AS student, p.course
 FROM passedCourses AS p, courses AS c;
 
-DROP VIEW UnreadMandatory CASCADE;
 
 
 CREATE VIEW PathToGraduation AS
-WITH calculations AS (SELECT Students.idnr AS student,
-           COALESCE(SUM(DISTINCT Courses.credits), 0) AS totalCredits,
+WITH calculations AS (SELECT basicInformation.idnr AS student,
+        --    SUM(credits) AS totalCredits 
+           COALESCE(SUM(DISTINCT passedCourses.credits), 0) AS totalCredits,
            COUNT(DISTINCT UnreadMandatory.course) AS mandatoryLeft,
-           SUM(DISTINCT CASE WHEN Classified.classification = 'math' THEN Courses.credits ELSE 0 END) AS mathCredits,
-           SUM(DISTINCT CASE WHEN Classified.classification = 'research' THEN Courses.credits ELSE 0 END) AS researchCredits,
+           SUM(DISTINCT CASE WHEN Classified.classification = 'math' THEN passedCourses.credits ELSE 0 END) AS mathCredits,
+           SUM(DISTINCT CASE WHEN Classified.classification = 'research' THEN passedCourses.credits ELSE 0 END) AS researchCredits,
            COUNT(DISTINCT CASE WHEN Classified.classification = 'seminar' THEN PassedCourses.course ELSE NULL END) AS seminarCourses
-    FROM Students
-    FULL OUTER JOIN PassedCourses ON Students.idnr = PassedCourses.student
-    left OUTER JOIN Courses ON PassedCourses.course = Courses.code
-    LEFT JOIN Classified ON Courses.code = Classified.course
-    LEFT JOIN UnreadMandatory ON Students.idnr = UnreadMandatory.student
-    GROUP BY Students.idnr),
-	calc AS (SELECT SUM(DISTINCT Courses.credits) AS recCredits
-	FROM Students
-	FULL OUTER JOIN PassedCourses ON Students.idnr = PassedCourses.student
-	left OUTER JOIN recommendedBranch ON PassedCourses.course = recommendedBranch.course
-	left OUTER JOIN Courses ON PassedCourses.course = Courses.code And recommendedBranch.course = Courses.code
+    FROM basicInformation
+    LEFT JOIN PassedCourses ON PassedCourses.student = basicInformation.idnr
+    LEFT JOIN Classified ON passedCourses.course = Classified.course
+    LEFT JOIN UnreadMandatory ON basicInformation.idnr = UnreadMandatory.student
+    GROUP BY basicInformation.idnr),
+	calc AS (
+SELECT StudentBranches.student as student, SUM(COALESCE(PassedCourses.credits,0)) as reccredits
+			FROM StudentBranches
+			LEFT JOIN RecommendedBranch ON (StudentBranches.branch, StudentBranches.program) = (RecommendedBranch.branch, RecommendedBranch.program)
+			LEFT JOIN  PassedCourses ON (StudentBranches.student, RecommendedBranch.course) = (PassedCourses.student, PassedCourses.course)	
+		GROUP BY StudentBranches.student
 	)
-    SELECT student, totalCredits, mandatoryLeft, mathCredits, researchCredits, seminarCourses,
-	(recCredits >= 10 AND mandatoryLeft = 0 AND mathCredits >= 20 AND researchCredits >= 10 AND seminarCourses >= 1) AS qualified
-	From calculations, calc;
+    SELECT calculations.student, totalCredits, mandatoryLeft, mathCredits, researchCredits, seminarCourses,
+	COALESCE((recCredits >= 10 AND mandatoryLeft = 0 AND mathCredits >= 20 AND researchCredits >= 10 AND seminarCourses >= 1),FALSE) AS qualified
+	From calculations 
+	LEFT JOIN calc
+	ON calculations.student = calc.student;
